31536c1ed37c9e8bdfc9297445c1f549cbca0aee
Choi-Eunseok <ted12333@gmail.com>
2025-05-16 23:59:29 +0900

    feature/result-page-draft

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@components': './components',
+            '@store': './store',
+            '@services': './services',
+            '@hooks': './hooks',
+          },
+        },
+      ],
+    ],
+  };
+};
